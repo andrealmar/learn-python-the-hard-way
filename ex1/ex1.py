b74e1cbaa9e86a61c643f853d786495b4242db17
Andre Almar <andre@y7mail.com>
2017-01-05 00:01:39 -0200

    adding ex1 code

---
+++ ex1/ex1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print "Hello World"
+print "Hello Again"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Printing'
+print "I'd much better rather you 'not'."
+print 'i "said" do not touch this.'
+print "testando utf 8 ççççc'áááééé"
