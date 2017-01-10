dd62ff3128d42e74138afa4023a1f888c219de88
Andre Almar <andre@y7mail.com>
2017-01-10 11:11:18 -0200

    lpthw ex10

---
+++ ex10/ex10.py
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in"
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+andre = "andre's car is an awesome machine"
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+print u'\U0001F47E'
+print "essa eh uma frase em ingles: %s" % andre
+
+# fun code
+# while True:
+#     for i in ["/", "-", "|", "\\", "|"]:
+#         print "%s\r" % i,
