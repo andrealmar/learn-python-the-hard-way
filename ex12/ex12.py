21e8904f8e9d2e464e39c5f4fda469fd81a82fe2
Andre Almar <andre@y7mail.com>
2017-01-10 18:26:02 -0200

    lpthw ex12

---
+++ ex12/ex12.py
@@ -0,0 +1,27 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weigth = raw_input("How much do you weigh? ")
+
+print "So you're %r years old, %r tall and %r heavy." % (age, height, weigth)
+
+# pydoc notes
+
+"""
+open - open a file and returns a file object. According to pydoc this is
+the preferred way to open a file
+"""
+
+"""
+file - module to work with files. You can write, read or append a file using
+this module
+"""
+
+"""
+os - a module for OS routines
+for NT or Posix depending on what system we're on.
+"""
+
+"""
+sys - This module provides access to some objects used or maintained by the
+    interpreter and to functions that interact strongly with the interpreter.
+"""
