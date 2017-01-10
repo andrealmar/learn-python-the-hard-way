e4f96d5565103a973b41db092f88be4ebd1489a5
Andre Almar <andre@y7mail.com>
2017-01-10 11:27:53 -0200

    lpthw ex11 added docs to input and raw_input

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -12,3 +12,9 @@ print "que time vc torce?",
 time = raw_input()
 
 print "voce torce para o %s" % time
+
+# input() documentation (python 3.6)
+# - https://docs.python.org/3/tutorial/inputoutput.html
+
+# raw_input() documentation (python 2.7)
+# - https://docs.python.org/2.7/tutorial/inputoutput.html
