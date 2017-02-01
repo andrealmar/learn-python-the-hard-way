548c69af9bb699e2deb66a66a01e3a0d18c579d7
Andre Almar <andre@y7mail.com>
2017-02-01 11:41:12 -0200

    lpthw ex33 refactor

--- ex33/ex33.py
+++ ex33/ex33.py
@@ -1,16 +1,19 @@
-i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+def num_append(head, tail):
+
+    for i in range(head, tail):
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+        print "The numbers: "
 
 
-print "The numbers: "
+num_append(0, 9)
 
 for num in numbers:
     print num
