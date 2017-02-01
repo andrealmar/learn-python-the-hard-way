dcabc124d17a73e7d2e2d377d30911e51f177f56
Andre Almar <andre@y7mail.com>
2017-02-01 18:41:20 -0200

    lpthw ex36

---
+++ ex36/ex36.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from sys import exit
+
+"""
+PORTA DOS DESESPERADOS - THE GAME
+"""
+
+print "há iéié meu gluglu"
+print "você está desesperado?"
+resposta = raw_input("> ")
+
+if resposta == "sim":
+    print "Então vamos começar o jogo...!!!"
+else:
+    print "Se você não está desesperado o que você está fazendo aqui?"
+    exit(0)
